1ab2c742d8a22b1634536f3f12a1cfbfa9160543
Mark R <mirubenstein@gmail.com>
2018-01-07 16:28:27 -0800

    Update based on tenebrousedge review

--- space_age.rb
+++ space_age.rb
@@ -1,6 +1,7 @@
 class SpaceAge
   YEAR_IN_SECONDS = 31_556_952.000_000_4
   EARTH_YEARS = {
+    earth: 1,
     jupiter: 11.862615,
     mars: 1.8808158,
     mercury: 0.2408467,
@@ -10,16 +11,14 @@ class SpaceAge
     venus: 0.61519726,
   }.freeze
 
-  def initialize(age_in_seconds)
-    @age_in_seconds = age_in_seconds
+  attr_reader :seconds
+
+  def initialize(seconds)
+    @seconds = seconds
   end
 
   EARTH_YEARS.each_pair do |planet, year|
-    define_method("on_#{planet}") { on_earth / year }
-  end
-
-  def on_earth
-    @age_in_seconds / YEAR_IN_SECONDS
+    define_method("on_#{planet}") { seconds / YEAR_IN_SECONDS / year }
   end
 end
 
